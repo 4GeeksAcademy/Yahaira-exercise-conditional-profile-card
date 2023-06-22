1d3af850f53ef0adbe7fce4d6a0afbef20144fee
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-06-22 03:20:24 +0000

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,39 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+          <h1>${variables.name == null ? "name" : variables.name} ${
+    variables.lastname == null ? "lastname" : variables.lastname
+  }</h1>
+          <h2>${variables.role == null ? "sin rol" : variables.role}</h2>
+           <h3>${
+             variables.country == null ? "without country" : variables.country
+           }, ${variables.city == null ? "without city" : variables.city}</h3>
+
+  <ul class="${
+    variables.socialMediaPosition == "position-left" ? "" : "position-right"
+  }">
+    <li>
+      <a href="${variables.twitter}"><i class="${
+    variables.twitter == null ? "fab fa-twitter" : "fab fa-twitter-square"
+  }"></i></a>
+    </li>
+    <li>
+      <a href="${variables.github}"><i class="${
+    variables.github == "alesanchezr" ? "fab fa-github" : "fab fa-github-square"
+  }"></i></a>
+    </li>
+    <li>
+      <a href="${variables.linkedin}"><i class="${
+    variables.linkedin == null ? "fab fa-linkedin" : "fab fa-linkedin-in"
+  }"></i></a>
+    </li>
+    <li>
+      <a href="${variables.instagram}"><i class="${
+    variables.instagram == null ? "fab fa-instagram" : "fab fa-instagram-square"
+  }"></i></a>
+    </li>
+  </ul>
+
         </div>
     `;
 }
